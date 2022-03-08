269102b28d19b4852f20e231ebc90976de829d65
Marie B <marie-jo-becker@t-online.de>
2022-03-08 23:23:42 +0100

    port change

--- 03_express/server.js
+++ 03_express/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const fileUpload = require('express-fileupload')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(fileUpload());
 
